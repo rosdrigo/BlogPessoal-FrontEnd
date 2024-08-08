3177f6c9710018eb144d23b2f17ddc497a6fd0ba
Rosdrigo <rosdrigol.tech@gmail.com>
2024-08-08 10:36:29 -0300

    add api conection

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home/Home';
+
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/Home/Home';
+import { AuthProvider } from './Contexts/AuthContext';
+import ListaTemas from './components/temas/listaTemas/ListaTemas';
+
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-        <Navbar />
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
           <div className='min-h-[80vh]'>
             <Routes>
-              <Route path="/" element={<Home />} />
+              <Route path="/" element={<Login />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
-);
+  );
 }
 export default App;
